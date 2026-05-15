8d8b6455eb81c5c585431e3e48c36ac38847e18b
Kaki Ryan <kakiryan@cs.unc.edu>
2026-05-15 08:11:32 -0400

    add anotoher file to lec02

---
+++ lectures/src/lec02/Absol.java
@@ -0,0 +1,18 @@
+package lec02;
+
+// Stack/heap memory exercise
+public class Absol {
+    public static void main(String[] args) {
+        int attack = 130;
+        String[] moves = {"Slash", "swordsDance"};
+        megaEvolve();
+    }
+    public static void megaEvolve() {
+        int attack = 150;
+        megaEvolveZ();
+        String extraMove = "phantomForce";
+    }
+    public static void megaEvolveZ() {
+        int speed = 151;
+    }
+}
